1b706d7054492fef7adcb702f2dede14580301c8
natcu <pulpul7@naver.com>
2026-05-22 19:52:38 +0900

    차임벨추가

--- cache/background_music_browser/Default/Extensions/pgbmogfigpngnbilikhdjphemfpdmnhl/2.0.0_0/init.js
+++
@@ -1,6 +0,0 @@
-const regExp = /(?=.*sidebar)(?=.*naver-webtoon)/;
-const ua = navigator.userAgent;
-
-if (regExp.test(ua)) {
-    document.documentElement.classList.add("_naver_webtoon_extension");
-}
